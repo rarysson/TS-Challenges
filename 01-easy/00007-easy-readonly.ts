60f8f166d1a7430bb5b268c511fefdcbbf3d72ac
Rarysson Guilherme <r_guilherme12@hotmail.com>
2023-11-14 22:59:43 -0300

    solution: 7

--- 01-easy/00007-easy-readonly.ts
+++ 01-easy/00007-easy-readonly.ts
@@ -31,7 +31,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyReadonly<T> = any
+type MyReadonly<Type> = {
+  readonly [Property in keyof Type]: Type[Property]
+}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
