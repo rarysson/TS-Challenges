3ff26e63c34330faf94e5aa14fbd84a427b4560a
Rarysson Guilherme <r_guilherme12@hotmail.com>
2023-11-15 21:57:54 -0300

    solution: 268

--- 01-easy/00268-easy-if.ts
+++ 01-easy/00268-easy-if.ts
@@ -19,18 +19,20 @@
 
 /* _____________ Your Code Here _____________ */
 
-type If<C, T, F> = any
+type If<Condition extends boolean, Truthy, Falsy> = Condition extends true
+	? Truthy
+	: Falsy;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
-  Expect<Equal<If<false, 'a', 2>, 2>>,
-]
+	Expect<Equal<If<true, "a", "b">, "a">>,
+	Expect<Equal<If<false, "a", 2>, 2>>
+];
 
 // @ts-expect-error
-type error = If<null, 'a', 'b'>
+type error = If<null, "a", "b">;
 
 /* _____________ Further Steps _____________ */
 /*
--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Solutions to the [type challenges](https://github.com/type-challenges/type-chall
 	- [x] 00018 - length of tuple
 	- [x] 00043 - exclude
 	- [x] 00189 - awaited
-	- [ ] 00268 - if
+	- [x] 00268 - if
 	- [ ] 00533 - concat
 	- [ ] 00898 - includes
 	- [ ] 03057 - push
