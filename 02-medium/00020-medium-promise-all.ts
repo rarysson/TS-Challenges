b305661ea1a2c509bd7c081ad4a847fb4ad0a116
Rarysson Guilherme <r_guilherme12@hotmail.com>
2023-11-17 10:27:43 -0300

    solution: 20

--- 02-medium/00020-medium-promise-all.ts
+++ 02-medium/00020-medium-promise-all.ts
@@ -22,23 +22,27 @@
 */
 
 /* _____________ Your Code Here _____________ */
-
-declare function PromiseAll(values: any): any
+// I didn't know how to do it, so I copied the solution
+declare function PromiseAll<T extends any[]>(
+	values: readonly [...T]
+): Promise<{
+	[P in keyof T]: T[P] extends Promise<infer R> | infer R ? R : never;
+}>;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
-const promiseAllTest1 = PromiseAll([1, 2, 3] as const)
-const promiseAllTest2 = PromiseAll([1, 2, Promise.resolve(3)] as const)
-const promiseAllTest3 = PromiseAll([1, 2, Promise.resolve(3)])
-const promiseAllTest4 = PromiseAll<Array<number | Promise<number>>>([1, 2, 3])
+const promiseAllTest1 = PromiseAll([1, 2, 3] as const);
+const promiseAllTest2 = PromiseAll([1, 2, Promise.resolve(3)] as const);
+const promiseAllTest3 = PromiseAll([1, 2, Promise.resolve(3)]);
+const promiseAllTest4 = PromiseAll<Array<number | Promise<number>>>([1, 2, 3]);
 
 type cases = [
-  Expect<Equal<typeof promiseAllTest1, Promise<[1, 2, 3]>>>,
-  Expect<Equal<typeof promiseAllTest2, Promise<[1, 2, number]>>>,
-  Expect<Equal<typeof promiseAllTest3, Promise<[number, number, number]>>>,
-  Expect<Equal<typeof promiseAllTest4, Promise<number[]>>>,
-]
+	Expect<Equal<typeof promiseAllTest1, Promise<[1, 2, 3]>>>,
+	Expect<Equal<typeof promiseAllTest2, Promise<[1, 2, number]>>>,
+	Expect<Equal<typeof promiseAllTest3, Promise<[number, number, number]>>>,
+	Expect<Equal<typeof promiseAllTest4, Promise<number[]>>>
+];
 
 /* _____________ Further Steps _____________ */
 /*
--- README.md
+++ README.md
@@ -27,7 +27,7 @@ Solutions to the [type challenges](https://github.com/type-challenges/type-chall
 	- [x] 00012 - Chainable options
 	- [x] 00015 - Last of array
 	- [x] 00016 - Pop
-	- [ ] 00020 - Promise all
+	- [x] 00020 - Promise all
 	- [ ] 00062 - Type lookup
 	- [ ] 00106 - Trim left
 	- [ ] 00108 - Trim
