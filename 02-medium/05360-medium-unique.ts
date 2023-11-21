4c4f2a8acd441ef778e86679bc83fe1f28339c4c
Rarysson Guilherme <r_guilherme12@hotmail.com>
2023-11-21 20:02:59 -0300

    5260

--- 02-medium/05360-medium-unique.ts
+++ 02-medium/05360-medium-unique.ts
@@ -20,18 +20,44 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Unique<T> = any
+type Includes<Type extends readonly any[], Value> = Type extends readonly [
+	infer Head,
+	...infer Tail
+]
+	? Equal<Head, Value> extends true
+		? true
+		: Includes<Tail, Value>
+	: false;
+
+type Unique<T, A extends readonly any[] = []> = T extends [
+	infer Head,
+	...infer Tail
+]
+	? Includes<A, Head> extends false
+		? Unique<Tail, [...A, Head]>
+		: Unique<Tail, A>
+	: A;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<Unique<[1, 1, 2, 2, 3, 3]>, [1, 2, 3]>>,
-  Expect<Equal<Unique<[1, 2, 3, 4, 4, 5, 6, 7]>, [1, 2, 3, 4, 5, 6, 7]>>,
-  Expect<Equal<Unique<[1, 'a', 2, 'b', 2, 'a']>, [1, 'a', 2, 'b']>>,
-  Expect<Equal<Unique<[string, number, 1, 'a', 1, string, 2, 'b', 2, number]>, [string, number, 1, 'a', 2, 'b']>>,
-  Expect<Equal<Unique<[unknown, unknown, any, any, never, never]>, [unknown, any, never]>>,
-]
+	Expect<Equal<Unique<[1, 1, 2, 2, 3, 3]>, [1, 2, 3]>>,
+	Expect<Equal<Unique<[1, 2, 3, 4, 4, 5, 6, 7]>, [1, 2, 3, 4, 5, 6, 7]>>,
+	Expect<Equal<Unique<[1, "a", 2, "b", 2, "a"]>, [1, "a", 2, "b"]>>,
+	Expect<
+		Equal<
+			Unique<[string, number, 1, "a", 1, string, 2, "b", 2, number]>,
+			[string, number, 1, "a", 2, "b"]
+		>
+	>,
+	Expect<
+		Equal<
+			Unique<[unknown, unknown, any, any, never, never]>,
+			[unknown, any, never]
+		>
+	>
+];
 
 /* _____________ Further Steps _____________ */
 /*
--- README.md
+++ README.md
@@ -80,7 +80,7 @@ Solutions to the [type challenges](https://github.com/type-challenges/type-chall
 	- [ ] 05153 - Indexof
 	- [x] 05310 - Join
 	- [ ] 05317 - Lastindexof
-	- [ ] 05360 - Unique
+	- [x] 05360 - Unique
 	- [ ] 05821 - Maptypes
 	- [ ] 07544 - Construct tuple
 	- [ ] 08640 - Number range
